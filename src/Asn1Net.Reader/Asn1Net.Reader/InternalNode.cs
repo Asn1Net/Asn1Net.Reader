cb2997f315012f220926b04533b3219c13cfdc94
Peter Polačko <peter.polacko@gmail.com>
2015-02-26 21:13:44 +0100

    license header update

--- src/Asn1Net.Reader/Asn1Net.Reader/InternalNode.cs
+++ src/Asn1Net.Reader/Asn1Net.Reader/InternalNode.cs
@@ -1,14 +1,14 @@
-﻿// /*
-//  *  Asn1Net.Reader - Managed ASN.1 Parsing library
-//  *  Copyright (c) 2014-2015 Peter Polacko
-//  *  Author: Peter Polacko <peter.polacko+asn1net@gmail.com>
-//  *
-//  *  Licensing for open source projects:
-//  *  Asn1Net.Reader is available under the terms of the GNU Affero General 
-//  *  Public License version 3 as published by the Free Software Foundation.
-//  *  Please see <http://www.gnu.org/licenses/agpl-3.0.html> for more details.
-//  *
-//  */
+﻿/*
+ *  Asn1Net.Reader - Managed ASN.1 Parsing library
+ *  Copyright (c) 2014-2015 Peter Polacko
+ *  Author: Peter Polacko <peter.polacko+asn1net@gmail.com>
+ *  
+ *  Licensing for open source projects:
+ *  Asn1Net.Reader is available under the terms of the GNU Affero General 
+ *  Public License version 3 as published by the Free Software Foundation.
+ *  Please see <http://www.gnu.org/licenses/agpl-3.0.html> for more details.
+ *
+ */
 
 using System;
 using System.Collections.Generic;
